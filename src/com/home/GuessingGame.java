7c9cc9647a36c1d65a950805f3db5cbc7729ae9a
solaris2006 <cristian.stefan@gmail.com>
2021-01-21 08:20:04 +0200

    Initial commit

---
+++ src/com/home/GuessingGame.java
@@ -0,0 +1,42 @@
+package com.home;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+        int tries = 0;
+        int prevNum = 0;
+        int secretNum = (int) Math.floor((Math.random() * 20) + 1);
+        System.out.println("Please guess the secret number between 1 and 20");
+
+        int i = 0;
+
+        while (isRunning) {
+            int num = sc.nextInt();
+            if (num < secretNum) {
+                System.out.println("The number is to low. Try again");
+                if (num != prevNum) {
+                    prevNum = num;
+                    tries++;
+                }
+            } else if (num > secretNum) {
+                System.out.println("The number is too high . Try again");
+                if (num != prevNum) {
+                    prevNum = num;
+                    tries++;
+                }
+            } else {
+                System.out.println("You have guessed the number");
+                if (num != prevNum) {
+                    prevNum = num;
+                    tries++;
+                }
+                isRunning = false;
+            }
+        }
+        System.out.println("The number of tries were " + tries);
+    }
+}
