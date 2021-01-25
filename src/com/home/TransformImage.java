af5283a1541bebfd736299d70e81b5cae464a0f7
solaris2006 <cristian.stefan@gmail.com>
2021-01-25 15:50:21 +0200

    add TransformImage class

---
+++ src/com/home/TransformImage.java
@@ -0,0 +1,24 @@
+package com.home;
+
+import ro.scoalainformala.image.ImageTransformation;
+
+import java.util.Scanner;
+
+
+public class TransformImage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please download an image file ");
+        System.out.println("Please enter the absolute path to downloaded file (eg C\\Users\\file\\...)");
+        String temp = sc.nextLine();
+        System.out.println("Please enter the name of file ");
+        String fileName = sc.nextLine();
+        String location = temp.replace("\\", "\\\\") + "\\";
+        String transformedName = "img2.png"; // it should be implemented as a regex
+        System.out.println("Please enter rotation degree");
+        int degrees = sc.nextInt();
+        ImageTransformation.rotate(location + fileName, location + transformedName, degrees);
+        System.out.println("You can find your  image at " + location.replace("\\\\", "\\") + transformedName);
+    }
+}
