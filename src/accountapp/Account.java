b00fc919c81f33457a732bf83172097b329f1e21
solaris2006 <cristian.stefan@gmail.com>
2021-03-07 19:01:11 +0200

    add Account class

---
+++ src/accountapp/Account.java
@@ -0,0 +1,42 @@
+package accountapp;
+
+public abstract class Account implements Printable, Deposit{
+    private String description;
+    private String iban;
+    private double balance;
+
+
+    public Account(String description, String iban, double balance) {
+        this.description = description;
+        this.iban = iban;
+        this.balance = balance;
+
+    }
+
+
+    public abstract void deposit(double amount);
+    public abstract void print();
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
